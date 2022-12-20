dcc0325fcaca94b6d7b1060af1f0227339e5c9f4
Saloh Karimov <karimov1994@gmail.com>
2022-12-20 09:32:51 +0800

    updated install_website.sh

--- install_website.sh
+++ install_website.sh
@@ -3,7 +3,7 @@ sudo su
 yum update -y
 yum install -y httpd
 cd /var/www/html
-wget https://github.com/azeezsalu/techmax/archive/refs/heads/main.zip
+wget https://github.com/Saloh-Karimov/techmax/archive/refs/heads/main.zip
 unzip main.zip
 cp -r techmax-main/* /var/www/html/
 rm -rf techmax-main main.zip
